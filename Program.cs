6c88ffed206266cda17ba5aac6c48553f4f91795
zelih <zelih@LAPTOP-Q0RFH5D0>
2022-10-13 11:34:13 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_log
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Loglama();
+            Console.ReadLine();
+        }
+
+        private static void Loglama()
+        {
+          try
+            {
+                Console.WriteLine("Bir sayı giriniz.");
+                int sayi=Convert.ToInt32(Console.ReadLine());
+            }
+        }
+    }
+}
